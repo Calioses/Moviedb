79af7446b6e31461b276ecce72254dba03185049
Monkeydew <55723201+Calioses@users.noreply.github.com>
2020-03-06 16:19:54 -0500

    Following course

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const welcome = "Welcome to React";
+class Welcome extends Component {
+    render() {
+        return(
+            <h1 className="App-title">{welcome}</h1>
+        )
+    }
+}
+
 class App extends Component {
     render() {
         return ( <div className = "App" >
@@ -10,6 +19,7 @@ class App extends Component {
             className = "App-logo"
             alt = "logo" / >
             <p>
+                <Welcome/>
             Edit < code > src / App.js </code> and save to reload. </p > 
             <a className = "App-link"
             href = "https://reactjs.org"
@@ -20,4 +30,5 @@ class App extends Component {
         );
     }
 }
+
 export default App;
