212459ddffdd35f4f9760befe6b6b996018f8d9c
Monkeydew <55723201+Calioses@users.noreply.github.com>
2020-03-09 23:22:08 -0400

    step 4 done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const welcome = "Welcome to React";
+const welcome = "Welcome to using props";
 class Welcome extends Component {
+    
     render() {
+        const { text, toggle } = this.props;
+        
+        
         return(
             <h1 className="App-title">{welcome}</h1>
         )
@@ -12,20 +16,35 @@ class Welcome extends Component {
 }
 
 class App extends Component {
+
+    state = {
+        toggle: true
+    }
+
+    toggle = () => {
+        this.setState({
+            toggle: !this.state.toggle
+        })
+    }
+
     render() {
         return ( <div className = "App" >
             <header className = "App-header" >
             <img src = { logo }
             className = "App-logo"
-            alt = "logo" / >
+            alt = "logo" />
             <p>
-                <Welcome/>
+                <Welcome text = "Welcome to Using Props" toggle ={this.state.toggle}/>
             Edit < code > src / App.js </code> and save to reload. </p > 
             <a className = "App-link"
             href = "https://reactjs.org"
             target = "_blank"
             rel = "noopener noreferrer" >
-            Learn React </a> 
+            Learn React </a>
+            <button onClick={this.toggle}>Show / Hide</button> 
+            {this.state.toggle && <p>This should show and hide</p>}
+             
+           
             </header > </div>
         );
     }
