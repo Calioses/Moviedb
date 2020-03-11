33fa72ccd8b8458336ddba811bbbfc00722437f3
Monkeydew <55723201+Calioses@users.noreply.github.com>
2020-03-11 14:12:26 -0400

    finished 14 fixed all files

--- src/App.js
+++ src/App.js
@@ -1,60 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from "./Movie";
+import { mockComponent } from 'react-dom/test-utils';
 
-const welcome = "Welcome to using props";
-class Welcome extends Component {
-    
-    render() {
-    const { text } = this.props;
-        return(
-            <h1 className="App-title">{welcome}</h1>
-        )
-    }
-}
 
 class App extends Component {
-    state = {
-        input: "Hello"
-    }
-    updateInput = (event) => {
-        console.log(event.target.value);
-        this.setState({
-            input: event.target.value.trim()
-        })
-    }
+   constructor(props){
+       super(props)
+       this.state={
+           movies: []
+       };
+   }
+   async componentDidMount(){
+        try {
+            const res = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=cd83def55682ae0a7c7d397f922c66c6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1");
 
-    submit = () => {
-        console.log(this.text)
-        
-    }
+            const movies = await res.json();
+            console.log(movies);
+            this.setState({
+                movies: movies.results
 
+
+            })
+            
+
+            
+        }catch(e){
+            console.log(e);
+        }
+    }
+    
     render() {
+        
         return ( <div className = "App" >
             <header className = "App-header" >
             <img src = { logo }
-            className = "App-logo"
+            className = " App-logo"
             alt = "logo" />
-            <p>
-                <Welcome text = "Welcome to Using Props"/>
-            Edit < code > src / App.js </code> and save to reload. </p > 
-            <a className = "App-link"
-            href = "https://reactjs.org"
-            target = "_blank"
-            rel = "noopener noreferrer" >
-            Learn React </a> 
-            <p className="App-intro"></p>
-            <h3>{this.state.input}</h3>
-            <input type="text" onChange={this.updateInput} value={this.state.input}/>
-            <input type="text" ref={(input) => this.text = input}/>
-            
-            
-            <button onClick={this.submit}> Show Value</button> 
-             
            
-            </header > </div>
+            </header>
+            {this.state.movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
+        
+            </div>
         );
-    }
+    } 
 }
 
 export default App;
